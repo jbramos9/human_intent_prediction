0ada185ee1be964c06f583420f8f9b1222e9236a
Joshua Ramos <113429431+jbramos9@users.noreply.github.com>
2023-07-04 21:05:44 +0800

    Add files via upload

---
+++ modified_detr/test.py
@@ -0,0 +1,25 @@
+import yaml
+from lightnings import LITGaMer, get_datamodule
+import lightning.pytorch as pl
+from trainer import get_logger
+
+def test():
+    with open("config.yaml", 'r') as f:
+        config = yaml.safe_load(f)
+
+    config["batch_size"] = 20
+    config["num_workers"] = 16
+    config["run_name"] = "default"
+
+    datamodule = get_datamodule(config)
+    model = LITGaMer.load_from_checkpoint("checkpoints/with_head_pretrained.ckpt")
+
+    trainer = pl.Trainer(
+        accelerator=config['accelerator'],
+        devices=[0],
+        # logger=get_logger(config),
+    )
+    trainer.test(model, datamodule=datamodule)
+
+if __name__=="__main__":
+    test()
